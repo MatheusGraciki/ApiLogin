50af3953efe9288a6b1fa67be571f354a1e0a6ac
Matheus Graciki <85004422+MatheusGraciki@users.noreply.github.com>
2023-02-12 19:50:24 -0300

    adds user registration and login with bcrypt password encryption in user controller

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,24 +1,42 @@
-import { User } from "../models/UserModel";
+import { UserModel } from "../models/UserModel";
 import { Request, Response } from "express";
+import bcrypt from 'bcrypt';
 
-class UserController {
+const UserController = {
+
     async registerUser(req: Request, res: Response) {
-        const user = {
-            email: req.body.email,
-            password: req.body.password
-        };
+        const { email, password } = req.body;
+        const existingUser = await UserModel.findOne({ email });
+        if (existingUser) {
+            return res.status(400).json({ msg: "Email já cadastrado - Tente fazer login ou utilize outro email" });
+        }
         try {
-            const existentAccount = await User.findOne(user);
-            if (existentAccount) {
-                return res.status(400).json({ msg: "Email já cadastrado - Tente fazer login ou utilize outro email" });
+            // eslint-disable-next-line @typescript-eslint/no-unused-vars
+            const createUserAccount = await UserModel.create({ email, password});
+            return res.status(201).json({ msg: "Conta criada com sucesso!"});
+            
+        } catch (error) {
+            return res.status(500).json({ msg: "Falha ao criar a conta.", error });
+        }
+    },
+
+    async loginUser(req: Request, res: Response) {
+        const { email, password } = req.body;
+        const user = await UserModel.findOne({ email });
+        if (!user) {
+            return res.status(404).json({ msg: "O email não foi encontrado" });
+        }
+        try {
+            const passwordIsValid = await bcrypt.compare(password, user.password);
+            if (!passwordIsValid) {
+                return res.status(401).json({ msg: "Senha inválida" });
             }
-            const createAccount = await User.create(user);
-            return res.status(201).json({ msg: "Conta criada com sucesso!", createAccount });
+            else{return res.status(200).json({ msg: "Login realizado com sucesso!" });}
         }
         catch (error) {
-            return res.status(500).json({ msg: "Erro ao criar conta.", error });
+            return res.status(500).json({ msg: "Erro ao fazer login.", error });
         }
     }
-}
+};
 
-export const userController = new UserController();
+export default UserController;
